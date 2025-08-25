df641da2f03b9e4ab52dd5f180560c9c9529a315
Cristopher Garcia <garcacristopher3@gmail.com>
2025-08-24 22:29:43 -0500

    Css modified and buttons added

--- crack-coding/src/views/Home.jsx
+++ crack-coding/src/views/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../index.css'
+import { IoPlayOutline } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -8,9 +10,20 @@ const Home = () => {
         <h1 className='home-title'>
           Domina la Programación <br/>con Retos Reales
         </h1>  
-        <p className='inro-text'>Mejora tus habilidades de programación resolviendo desafíos 
-          algorítmicos, aprende<br/> nuevas técnicas y conecta con una comunidad de desarrolladores apasionados.
+        <p className='intro-text'>Mejora tus habilidades de programación resolviendo desafíos 
+          algorítmicos, aprende nuevas técnicas y conecta con una comunidad de desarrolladores apasionados.
         </p>  
+        <div className='home-buttons'>
+          <button className='btn-get-started'>
+          <IoPlayOutline /><span>Comenzar Ahora </span>
+          
+        </button>
+        <button className='btn-sign-up'>
+          Crear Cuenta Gratis
+          <FaArrowRight />
+        </button>
+        </div>
+        
     </div>
    </>
   )
