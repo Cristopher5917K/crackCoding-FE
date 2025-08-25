ccc44c921b754cae3c5d25179b0dd510ff93a2f5
Cristopher Garcia <garcacristopher3@gmail.com>
2025-08-24 21:46:39 -0500

    Added Home text

--- crack-coding/src/views/Home.jsx
+++ crack-coding/src/views/Home.jsx
@@ -4,10 +4,14 @@ import '../index.css'
 const Home = () => {
   return (
    <>
-
-    <h7 className='text-3xl font-bold text-center mt-10'>
-      Welcome to Crack Coding
-    </h7>
+    <div className="text-center mt-5">
+        <h1 className='home-title'>
+          Domina la Programación <br/>con Retos Reales
+        </h1>  
+        <p className='inro-text'>Mejora tus habilidades de programación resolviendo desafíos 
+          algorítmicos, aprende<br/> nuevas técnicas y conecta con una comunidad de desarrolladores apasionados.
+        </p>  
+    </div>
    </>
   )
 }
