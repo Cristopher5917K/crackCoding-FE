0e3573cd10f32943b18378b2cf2d90a99c5ac2b4
Cristopher Garcia <garcacristopher3@gmail.com>
2025-08-27 21:41:21 -0500

    Added register inputs

--- crack-coding/src/views/Register.jsx
+++ crack-coding/src/views/Register.jsx
@@ -42,7 +42,43 @@ const Register = () => {
       }
     }
   return (
-    <div>Register</div>
+    <div>
+        <h2>Register</h2>
+        <h3>Test store: {store.personas}</h3>
+        <form>
+        <div>
+            <label htmlFor="name">Nombre:</label>
+            <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            />
+        </div>
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            />
+        </div>
+        <div>
+            <label htmlFor="password">Contraseña:</label>
+            <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            />
+        </div>
+        </form>
+        <button onClick={handleRegister}>Register</button>
+    </div>
   )
 }
 
