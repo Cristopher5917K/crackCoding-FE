a62276c437895d041b24420363dece62fffb023a
Cristopher Garcia <garcacristopher3@gmail.com>
2025-09-15 22:44:55 -0500

    Added login inputs

--- crack-coding/src/views/Login.jsx
+++ crack-coding/src/views/Login.jsx
@@ -24,15 +24,53 @@ const Login = () => {
     //   }; 
 
     return (
-    <div className='login-container'>
-        <div className='back-home'>
-            <a href='/'>
-                <FaLongArrowAltLeft />
-                <span>Volver a Inicio</span>
-            </a>
+        <div className='login-container'>
+            <div className='back-home'>
+                <a href='/'>
+                    <FaLongArrowAltLeft />
+                    <span>Volver a Inicio</span>
+
+                </a>
+            </div>
+            <div>
+                <h2>Login</h2>
+                <h3>Test store: {store.personas}</h3>
+                <form>
+                    <div>
+                        <label htmlFor="name">Nombre:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Contraseña:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </form>
+                <button onClick={handleRegister}>Register</button>
+            </div>
+
         </div>
-        
-    </div>
     );
 };
 
